863a5a640dbb4849469c42a729ed927ce7da38a8
jkh0515 <81910107+jkh0515@users.noreply.github.com>
2023-09-30 02:05:25 +0900

    Create pg155652.cpp

---
+++ ProgrammersFile/pg155652.cpp
@@ -0,0 +1,33 @@
+//둘만의 암호
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    string skip;
+    int index;
+    cin >> s >> skip >> index;
+    for(int i=0;i<s.length();i++) {
+        for(int j=0;j<index;j++) {
+            s[i] = s[i] + 1;
+            if(s[i] == 123) {
+                s[i] = 97;
+            }
+            for(int k=0;k<skip.length();k++) {
+                for(int l=0;l<skip.length();l++) {
+                    if(s[i] == skip[l]) {
+                        s[i] = s[i] + 1;
+                        if(s[i] == 123) {
+                            s[i] = 97;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << s;
+    return 0;
+}
