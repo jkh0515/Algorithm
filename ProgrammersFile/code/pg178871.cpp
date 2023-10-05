a3d259aef1407ecb7a6dccc2dbf4e32c280784c5
jkh0515 <81910107+jkh0515@users.noreply.github.com>
2023-10-05 22:16:09 +0900

    Update pg178871.cpp

--- ProgrammersFile/code/pg178871.cpp
+++ ProgrammersFile/code/pg178871.cpp
@@ -1,5 +1,5 @@
 //달리기 경주
-//수정중 - 미완성
+//수정중222 - 미완성
 
 #include <string>
 #include <vector>
@@ -7,64 +7,60 @@ using namespace std;
 
 typedef struct node {
     string name;
-    node* prevNode;
-    node* nextNode;
+    int now;
+    int nowIdx;
+    int pIdx;
+    int nIdx;
 } node;
 
-node* createNode(string name, node* prevNode, node* nextNode) {
-    node* newNode = new node;
-    newNode->name = name;
-    newNode->prevNode = prevNode;
-    newNode->nextNode = nextNode;
-    return newNode;
-}
-
-long hashing(string name) {
-    long value = 0;
-    for (int i = 0; i < name.length(); i++) {
-        value = ((value * 31) * (name[i] - 'a' + 1)) % 50032;
+int hsh(string name) {
+    int value = 0;
+    for(int i=0;i<name.size();i++) {
+        value = (value * 29 + name[i]) % 50032;
     }
     return value;
 }
 
-vector<string> solution(vector<string> players, vector<string> callings) {
-    node* ground[50023] = {nullptr};
-    node* headNode = new node;
-    node* tailNode = new node;
-    node* firstNode = new node;
-    node* lastNode = new node;
-    headNode = createNode("-1", nullptr, firstNode);
-    firstNode = createNode(players[0], headNode, lastNode);
-    lastNode = createNode(players[players.size()-1], firstNode, tailNode);
-    tailNode = createNode("-1", lastNode, nullptr);
-    ground[hashing(players[0])] = firstNode;
-    ground[hashing(players[players.size()-1])] = lastNode;
+node* createNode(string name, int now, int nowIdx, int pIdx, int nIdx) {
+    node* newNode = new node;
+    newNode->name = name;
+    newNode->now = now;
+    newNode->nowIdx = nowIdx;
+    newNode->pIdx = pIdx;
+    newNode->nIdx = nIdx;
+    return newNode;
+}
+
+vector<string> solution(vector<string> pl, vector<string> ca) {
     vector<string> answer;
-    for(int i=1;i<players.size()-1;i++) {
-        node *playerNode;
-        playerNode = createNode(players[i], ground[hashing(players[i-1])], ground[hashing(players[i+1])]);
-        ground[hashing(players[i])] = playerNode;
+    int size = pl.size();
+    node* g[50034] = {nullptr};
+    node* hNode = createNode("-1", -1, 50032, -1, hsh(pl[0]));
+    node* lNode = createNode("-1", -1, 50033, hsh(pl[size-1]), -1);
+    node* fNode = createNode(pl[0], 0, hsh(pl[0]), 50032, hsh(pl[1]));
+    node* tNode = createNode(pl[size-1], size-1, hsh(pl[size-1]), hsh(pl[size-2]), -1);
+    g[50032] = hNode;
+    g[50033] = lNode;
+    g[hsh(pl[0])] = fNode;
+    g[hsh(pl[size-1])] = tNode;
+    for(int i=1;i<size-1;i++) {
+        node* pNode = createNode(pl[i], i, hsh(pl[i]), hsh(pl[i-1]), hsh(pl[i+1]));
+        g[hsh(pl[i])] = pNode;
     }
-    for(int i=0;i<callings.size();i++) {
-        if(ground[hashing(callings[i])] != nullptr) {
-            node* insertNode;
-            node* delNode;
-            node* nNode;
-            delNode = ground[hashing(callings[i])];
-            nNode = delNode->nextNode;
-            insertNode = createNode(callings[i], delNode->prevNode->prevNode, delNode->prevNode);
-            delNode->prevNode->prevNode->nextNode = insertNode;
-            delNode->prevNode->prevNode = insertNode;
-            insertNode->nextNode->nextNode = nNode;
-            nNode->prevNode = insertNode->nextNode;
-            delete delNode;
-            ground[hashing(callings[i])] = insertNode;
-        }
+    for(int i=0;i<ca.size();i++) {
+        node* curNode = g[hsh(ca[i])];
+        g[g[curNode->pIdx]->pIdx]->nIdx = hsh(ca[i]);
+        g[curNode->pIdx]->nIdx = curNode->nIdx;
+        int temp = g[curNode->pIdx]->pIdx;
+        g[curNode->nIdx]->pIdx = curNode->pIdx;
+        g[curNode->pIdx]->pIdx = hsh(ca[i]);
+        curNode->nIdx = curNode->pIdx;
+        curNode->pIdx = temp;
     }
-    node* outNode = headNode;
-    for(int i=0;i<players.size()+4;i++) {
-        answer.push_back(outNode->name);
-        outNode = outNode->nextNode;
+    node* tempNode = g[hNode->nIdx];
+    for(int i=0;i<size;i++) {
+        answer.push_back(tempNode->name);
+        tempNode = g[tempNode->nIdx];
     }
     return answer;
 }
