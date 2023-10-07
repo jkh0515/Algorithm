f483d307b76bce72b60f538fdaa5e279ff910079
jkh0515 <81910107+jkh0515@users.noreply.github.com>
2023-10-07 22:24:08 +0900

    Create pg12969.cpp

---
+++ ProgrammersFile/code/pg12969.cpp
@@ -0,0 +1,15 @@
+//직사각형 별찍기
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin >> a >> b;
+    for(int i=0;i<b;i++) {
+        for(int j=0;j<a;j++)
+            cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
