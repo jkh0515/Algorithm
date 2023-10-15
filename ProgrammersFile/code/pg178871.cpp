bad27562ebd165a70fb6c3e29102c12f1032ee3e
jkh0515 <81910107+jkh0515@users.noreply.github.com>
2023-10-15 23:31:39 +0900

    Update pg178871.cpp

--- ProgrammersFile/code/pg178871.cpp
+++ ProgrammersFile/code/pg178871.cpp
@@ -1,72 +1,25 @@
 //달리기 경주
-//수정중4444 - 미완성
 
 #include <string>
 #include <vector>
+#include <map>
+#include <iostream>
 using namespace std;
 
-const int M = 134180233;
-
-typedef struct node {
-    string name;
-    int now;
-    int nowIdx;
-    int pIdx;
-    int nIdx;
-} node;
-
-int hsh(string name) {
-    long long value = 0;
-    long long r = 3;
-    for(int i=0; i<name.size(); i++){
-        value = (value + (name[i] - 96) * r) % M;
-        r = (r * 31) % M;
-    }
-    return value;
-}
-
-
-node* createNode(string name, int now, int nowIdx, int pIdx, int nIdx) {
-    node* newNode = new node;
-    newNode->name = name;
-    newNode->now = now;
-    newNode->nowIdx = nowIdx;
-    newNode->pIdx = pIdx;
-    newNode->nIdx = nIdx;
-    return newNode;
-}
-
 vector<string> solution(vector<string> pl, vector<string> ca) {
+    map<string, int> ma;
     vector<string> answer;
-    int size = pl.size();
-    node* g[M+2] = {nullptr};
-    node* hNode = createNode("-1", -1, 50032, -1, hsh(pl[0]));
-    node* lNode = createNode("-1", -1, 50033, hsh(pl[size-1]), -1);
-    node* fNode = createNode(pl[0], 0, hsh(pl[0]), M, hsh(pl[1]));
-    node* tNode = createNode(pl[size-1], size-1, hsh(pl[size-1]), hsh(pl[size-2]), M+1);
-    g[M] = hNode;
-    g[M+1] = lNode;
-    g[hsh(pl[0])] = fNode;
-    g[hsh(pl[size-1])] = tNode;
-    for(int i=1;i<size-1;i++) {
-        node* pNode = createNode(pl[i], i, hsh(pl[i]), hsh(pl[i-1]), hsh(pl[i+1]));
-        // while(g[hsh(pl[i])] != nullptr)
-        g[hsh(pl[i])] = pNode;
-    }
+    for(int i=0;i<pl.size();i++)
+        ma.insert(make_pair(pl[i], i));
     for(int i=0;i<ca.size();i++) {
-        node* curNode = g[hsh(ca[i])];
-        g[g[curNode->pIdx]->pIdx]->nIdx = hsh(ca[i]);
-        g[curNode->pIdx]->nIdx = curNode->nIdx;
-        int temp = g[curNode->pIdx]->pIdx;
-        g[curNode->nIdx]->pIdx = curNode->pIdx;
-        g[curNode->pIdx]->pIdx = hsh(ca[i]);
-        curNode->nIdx = curNode->pIdx;
-        curNode->pIdx = temp;
+        int idx = ma.find(ca[i])->second;
+        string temp = pl[idx-1];
+        ma.find(ca[i])->second--;
+        ma.find(temp)->second++;
+        pl[idx-1] = pl[idx];
+        pl[idx] = temp;
     }
-    node* tempNode = g[hNode->nIdx];
-    for(int i=0;i<size;i++) {
-        answer.push_back(tempNode->name);
-        tempNode = g[tempNode->nIdx];
-    }
+    for(int i=0;i<pl.size();i++)
+        answer.push_back(pl[i]);
     return answer;
 }
