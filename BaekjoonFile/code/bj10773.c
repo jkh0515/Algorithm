30203efac6168c9e0d8ecc2215c80e0a973c7339
jkh0515 <81910107+jkh0515@users.noreply.github.com>
2024-01-11 22:51:04 +0900

    Create bj10773.c

---
+++ BaekjoonFile/code/bj10773.c
@@ -0,0 +1,17 @@
+//제로 [ C ]
+
+#include <stdio.h>
+
+int main() {
+    int n, arr[100001] = {0}, idx = 0, answer = 0;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) {
+        scanf("%d", &arr[idx++]);
+        if(arr[idx-1] == 0 && idx > 0)
+            idx -= 2;
+    }
+    for(int i=0;i<idx;i++)
+        answer += arr[i];
+    printf("%d", answer);
+    return 0;
+}
